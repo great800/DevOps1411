f56e1c8909a43d8e0939897df7263153bc17318c
great800 <gr303040@gmail.com>
2021-12-26 18:37:07 +0200

    Create 14.py

---
+++ 14.py
@@ -0,0 +1,40 @@
+isTrue = False
+a = 2
+b = 2.5
+strOne = "One"
+strThree = "Three"
+
+if a >= 2 and (strOne == "One" or not strThree == "two"):
+    print("a is greater than 2")
+elif b == 2.5 or b == 2.5:
+    print("something")
+elif strOne == "3":
+    print("bla")
+else:
+    print("a is lower than 2")
+
+my_list = ["Hen", "Lior", "Shay", "Ariel"]
+my_other_list = []
+if 0 < a <=4:
+    print("Ok")
+
+if "Hen" in my_list:
+    print ("We have found Hen!")
+
+if my_other_list:
+    print ("Hello")
+
+if len(my_list) > 0:
+    print("hey")
+
+if len(my_other_list) > 0:
+    print("hey no")
+
+c = ["aaa" , "1"]
+d = ["aaa" , "1"]
+e = 9.0
+if c is d:
+    print("c is d")
+
+if type(e) is int:
+    print ("e is an integer")
